5bd58c95ded208d63cb1ced48bbc9301f094c394
Cait Payne <caitpayne@outlook.com>
2018-04-19 23:32:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ function increaseRankBy(n) {
 
 function deepestChild() {
 const child = document.getElementById('grand-node').querySelectorAll('div');
-const deepChild = child.children[0]
+const deepChild = child.children
 for (let i = 0; deepChild.children[i]; i) {
- const deepChild = deepChild.children[i]
+ const deepChild = deepChild.children[0]
 }
 return deepChild
 }
